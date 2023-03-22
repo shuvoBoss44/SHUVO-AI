0b8988391059275b41e2b5a2ee4d98f7334705b4
Shuvo Chakma <chakmashuvo2016@gmail.com>
2023-03-23 02:29:38 +0600

    changes

--- app.js
+++ app.js
@@ -29,6 +29,7 @@ app.post("/", async (req, res) => {
         res.status(200).send({
             bot: response.data.choices[0].text
         })
+        
     } catch (error) {
         // console.log(error);
         res.status(500).send({error})
